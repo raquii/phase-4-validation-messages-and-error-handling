d2b697a5d2e4fc593ef275ef1f6a79fa9f87b298
raquel <raquelanaroman@gmail.com>
2021-08-18 11:17:06 -0400

    commit tree ca2e52456bea4bf0c6477bc96d18c97ba33954f6

--- client/src/components/MovieForm.js
+++ client/src/components/MovieForm.js
@@ -14,6 +14,8 @@ function MovieForm() {
     female_director: false,
   });
 
+  const [errors, setErrors] = useState([]);
+
   function handleSubmit(e) {
     e.preventDefault();
     fetch("/movies", {
@@ -23,8 +25,13 @@ function MovieForm() {
       },
       body: JSON.stringify(formData),
     })
-      .then((response) => response.json())
-      .then((newMovie) => console.log(newMovie));
+      .then((response) => {
+        if (response.ok) {
+          response.json().then((newMovie) => console.log(newMovie));
+        } else {
+          response.json().then((errorData) => setErrors(errorData.errors));
+        }
+      })
   }
 
   function handleChange(e) {
@@ -38,6 +45,15 @@ function MovieForm() {
 
   return (
     <Wrapper>
+        {
+        errors.length > 0 && (
+          <ul style={{ color: "red" }}>
+            {errors.map((error) => (
+              <li key={error}>{error}</li>
+            ))}
+          </ul>
+        )
+          }
       <form onSubmit={handleSubmit}>
         <FormGroup>
           <label htmlFor="title">Title</label>
